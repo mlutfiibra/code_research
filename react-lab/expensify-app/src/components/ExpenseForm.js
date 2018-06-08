b44df2f5388f12ffb5cbfb182d4dcc9515981455
muhammad <mlutfiibra@outlook.com>
2018-06-09 00:10:47 +0700

    Implement React-form

--- react-lab/expensify-app/src/components/ExpenseForm.js
+++ react-lab/expensify-app/src/components/ExpenseForm.js
@@ -12,7 +12,7 @@ export default class ExpenseForm extends React.Component {
         description: '',
         note: '',
         amount: '',
-        cratedAt: moment(),
+        createdAt: moment(),
         calendarFocused: false
     };
     onDescriptionChange = (e) => {
@@ -26,20 +26,38 @@ export default class ExpenseForm extends React.Component {
     onAmountChange = (e) => {
         const amount = e.target.value;
   
-        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+        if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
           this.setState(() => ({ amount }));
         }
     };
     onDateChange = (createdAt) => {
-        this.setState(() => ({createdAt}));
+        if(createdAt) {
+            this.setState(() => ({createdAt}));
+        }
     };
     onFocusChange = ({focused}) => {
         this.setState(() => ({ calendarFocused: focused }));
     };
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        if(!this.state.description || !this.state.amount) {
+            this.setState({ error: 'Please tell me the description and amount' });
+        } else {
+            this.setState({ error: '' });
+            this.props.onSubmit({
+                description: this.state.description,
+                amount: parseFloat(this.state.amount, 10) * 100,
+                createdAt: this.state.createdAt.valueOf(),
+                note: this.state.note
+            });
+        }
+    };
     render() {
         return (
             <div>
-                <form>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.onSubmit}>
                     <input
                         type="text"
                         placeholder="Description"
@@ -54,7 +72,7 @@ export default class ExpenseForm extends React.Component {
                         onChange={this.onAmountChange}
                     />
                     <SingleDatePicker 
-                        date={this.state.cratedAt}
+                        date={this.state.createdAt}
                         onDateChange={this.onDateChange}
                         focused={this.state.calendarFocused}
                         onFocusChange={this.onFocusChange}
