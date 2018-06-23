76baaaa3c038c6b9b9048aa73d2350df9d62ae4f
muhammad <mlutfiibra@outlook.com>
2018-06-23 20:11:23 +0700

    Config webpack production mode

--- react-lab/expensify-app/src/app.js
+++ react-lab/expensify-app/src/app.js
@@ -24,8 +24,6 @@ store.dispatch(addExpense({ description: 'Rent', amount: 10000 }));
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
